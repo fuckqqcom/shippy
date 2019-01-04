59fb43be447576fb5671c9a835c0e9abca167aa9
95028555@qq.com <yangyi199159>
2019-01-04 15:26:50 +0800

    init

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"google.golang.org/grpc"
+	"github.com/micro/go-micro"
 	"log"
-	"net"
 	pb "shippy/consignment-service/proto/consignment"
 )
 
@@ -14,7 +13,8 @@ const (
 
 //仓库接口
 type IRepository interface {
-	Create(consignment *pb.ShippingServiceServer) (*pb.Consignment, error)
+	Create(consignment *pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 //存放多批货物的仓库,实现了IRepository接口
@@ -36,40 +36,53 @@ type service struct {
 	repo Repository
 }
 
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
 
 	//接收新的货物
 	consignment, err := s.repo.Create(req)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	resp := &pb.Response{Created: true, Consignment: consignment}
-	return resp, nil
+	resp = &pb.Response{Created: true, Consignment: consignment}
+	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, resp *pb.Response) error {
 	allConsignments := s.repo.GetAll()
-	resp := &pb.Response{Consignments: allConsignments}
-	return resp, nil
+	resp = &pb.Response{Consignments: allConsignments}
+	return nil
 }
 
 func main() {
-	listener, err := net.Listen("tcp", Port)
+	server := micro.NewService(
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
 
-	if err != nil {
-		log.Fatalf("failed to listen:%v", err)
-	}
-
-	log.Printf("listen on : %s \n", Port)
-
-	server := grpc.NewServer()
+	server.Init()
 	repo := Repository{}
+	pb.RegisterShippingServiceHandler(server.Server(), &service{repo: repo})
 
-	pb.RegisterShippingServiceServer(server, &service{repo})
-
-	if err := server.Serve(listener); err != nil {
-		log.Fatalf("failed to save :%v", err)
+	if err := server.Run(); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
+
+	//listener, err := net.Listen("tcp", Port)
+	//
+	//if err != nil {
+	//	log.Fatalf("failed to listen:%v", err)
+	//}
+	//
+	//log.Printf("listen on : %s \n", Port)
+	//
+	//server := grpc.NewServer()
+	//repo := Repository{}
+	//
+	//pb.RegisterShippingServiceServer(server, &service{repo})
+	//
+	//if err := server.Serve(listener); err != nil {
+	//	log.Fatalf("failed to save :%v", err)
+	//}
 }
