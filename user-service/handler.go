78436c19df74837e6f99758cb006ffb8258fc720
95028555@qq.com <yangyi199159>
2019-01-07 18:31:32 +0800

    proto

--- user-service/handler.go
+++ user-service/handler.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"context"
+	pb "shippy/user-service/proto/user"
+)
+
+type handler struct {
+	repo         Repository
+	tokenService Authable
+}
+
+func (h *handler) Create(ctx context.Context, req *pb.User, resp *pb.Response) error {
+
+	if err := h.repo.Create(req); err != nil {
+		return nil
+	}
+
+	resp.User = req
+	return nil
+}
+
+func (h *handler) Get(ctx context.Context, req *pb.User, resp *pb.Response) error {
+	u, err := h.repo.Get(req.Id)
+
+	if err != nil {
+		return err
+	}
+
+	resp.User = u
+	return nil
+}
+
+func (h *handler) GetAll(ctx context.Context, req *pb.Request, resp *pb.Response) error {
+	u, err := h.repo.GetAll()
+
+	if err != nil {
+		return err
+	}
+
+	resp.Users = u
+	return nil
+}
+
+func (s *handler) Auth(ctx context.Context, req *pb.User, resp *pb.Token) error {
+	_, err := s.repo.GetByEmailAndPassword(req)
+
+	if err != nil {
+		return err
+	}
+
+	resp.Token = "testing"
+	return nil
+}
+
+func (s *handler) ValidateToken(ctx context.Context, req *pb.Token, resp *pb.Token) error {
+	return nil
+}
--- user-service/repository.go
+++ user-service/repository.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"errors"
+	"github.com/xormplus/xorm"
+	pb "shippy/user-service/proto/user"
+)
+
+type Repository interface {
+	Get(id string) (*pb.User, error)
+	GetAll() ([]*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmailAndPassword(user *pb.User) (*pb.User, error)
+}
+
+type UserRepository struct {
+	db *xorm.Engine
+}
+
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
+
+	if err := repo.db.Find(&users).Error; err != nil {
+		return nil, errors.New(err())
+	}
+
+	return users, nil
+}
+
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
+	var user *pb.User
+	user.Id = id
+
+	if err := repo.db.Find(&user).Error; err != nil {
+		return nil, errors.New(err())
+	}
+
+	return user, nil
+}
+
+func (repo *UserRepository) GetByEmailAndPassword(user *pb.User) (*pb.User, error) {
+	if err := repo.db.Find(&user).Error; err != nil {
+		return nil, errors.New(err())
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) Create(user *pb.User) error {
+	if _, err := repo.db.Insert(user); err != nil {
+		return err
+	}
+
+	return nil
+}
